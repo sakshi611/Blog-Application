ee484a2abcf25e21be2f874e0e1e3d79ce549353
sakshi611 <sakshi.casper@gmail.com>
2022-10-06 12:39:06 -0700

    updated

--- src/main/java/com/BlogApplication/Controller/PostController.java
+++ src/main/java/com/BlogApplication/Controller/PostController.java
@@ -22,5 +22,26 @@ public class PostController {
         return new ResponseEntity<List<Post>>(postService.getAllPost(), HttpStatus.OK) ;
     }
 
+	    @GetMapping("/posts/{id}")
+	    public ResponseEntity<Post> getPostById(@PathVariable int id){
+	        return new ResponseEntity<>(postService.getPostById(id), HttpStatus.OK);
+	    }
 
+	    @PostMapping("/posts")
+	    public ResponseEntity<String> createPost(@RequestBody Post post){
+	        return new ResponseEntity<>(postService.createPost(post), HttpStatus.CREATED);
+	    }
+	    @PutMapping("/posts/{id}")
+	    public ResponseEntity<Post> updatePostById(@RequestBody Post post, @PathVariable int id){
+	        return new ResponseEntity<>(postService.updatePostById(post,id), HttpStatus.OK);
+	    }
+	    @DeleteMapping("/posts/{id}")
+	    public ResponseEntity<String> deletePostById(@PathVariable int id){
+	        return new ResponseEntity<String>(postService.deletePostById(id), HttpStatus.OK);
+	    }
+
+	    @GetMapping(value = "/posts/", params = {"pageNo", "pageSize"} )
+	    public ResponseEntity<List<Post>> pageAndSort(@RequestParam int pageNo, @RequestParam int pageSize, @RequestParam(required = false) String sortBy){
+	        return new ResponseEntity<List<Post>>(postService.pageAndSort(pageNo,pageSize,sortBy), HttpStatus.OK);
+	    }
 }
